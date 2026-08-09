f4f26b5b71ad851bbc5ffd356b260848cee5c1c1
Your Name <you@example.com>
2026-08-09 15:20:29 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::io;
+fn main() {
+    println!("Please enter a number");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {}", guess);
+}
